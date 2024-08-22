6b811d77797cada889b1f4454986737ced8d6ca7
ADVENTISTAS\lucas.andre <lucas.andre@unasp.edu.br>
2024-08-22 16:14:14 -0300

    prettier e eslint

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      globals: globals.node,
+      parser: "@typescript-eslint/parser",
+    },
+  },
+  pluginJs.configs.recommended,
+  tseslint.configs.recommended,
+  prettier,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+];
